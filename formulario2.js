87cfa4e6cdc6177947f084c429eb5f68acccf1de
juxo321 <juxo321@gmail.com>
2021-09-29 20:02:46 -0500

    Proyecto básico con integración de spark java

--- formulario2.js
+++ formulario2.js
@@ -29,3 +29,28 @@ function callback() {
     cambiarColor("green");
     cambiarEstado(false);
 }
+
+
+//Calculadora 
+
+  
+function insert(num) {
+    document.form.textview.value = document.form.textview.value + num;
+}
+
+function equal() {
+    let exp = document.form.textview.value;
+
+    if (exp) {
+        document.form.textview.value = eval(exp);
+    }
+}
+
+function clean() {
+    document.form.textview.value = '';
+}
+
+function back() {
+    let exp = document.form.textview.value;
+    document.form.textview.value = exp.substring(0, exp.length - 1);
+}
