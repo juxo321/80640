d269d2349e03295c64331b00441b5e1c627f65e9
juxo321 <juxo321@gmail.com>
2021-10-20 10:45:22 -0500

    Ejercicio en clase app-velocity

--- ejemploFormulario/formularioRegistro.js
+++ ejemploFormulario/formularioRegistro.js
@@ -2,25 +2,25 @@ var nombreP = document.getElementById("nombre").value;
 var constrasenaP = document.getElementById("contrasena").value;
 
 var btnRegistrarse = document.getElementById("btnRegistrarse");
-btnRegistrarse.addEventListener("click", function () {
+btnRegistrarse.addEventListener("click", click);
+
+function click(){
     var params = new URLSearchParams();
     var nombre = document.getElementById("nombre").value;
     var contrasena = document.getElementById("contrasena").value;
-    nombreP = nombre;
-    contrasenaP = contrasena;
     params.append("nombre", nombre);
     params.append("contrasena", contrasena);
     axios.get("http://localhost:4567/registrarUsuario?" +  params )
         .then(function (rs) {
-            //Aqui recibo el valor del servidor pero no se porque el valor de las variables no coambia y no lo puedo guardar en el metodo guardar() para agregarlo a la tabal
+            alert(rs.data.nombre)
+            //Aqui recibo el valor del servidor pero no se porque el valor de las variables no coambia y no lo puedo guardar en el metodo guardar() para agregarlo a la tabla
             nombreP = rs.data.nombre;
-            constrasenaP = rs.data.nombre;
+            constrasenaP = rs.data.contrasena;
+            alert(nombreP);
         })
         .catch(function (error) {
-    
         });
-}); 
-
+}
 
 function guardar(){
     var fila="<tr><td>"+nombreP+"</td><td>"+constrasenaP+"</td></tr>";
